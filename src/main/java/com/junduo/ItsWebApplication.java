c9d71115886e368e3070a33561bbaea0061626cf
JNbuck <1459719481@qq.com>
2022-01-23 16:08:52 +0800

    commit all

--- src/main/java/com/junduo/ItsWebApplication.java
+++ src/main/java/com/junduo/ItsWebApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// @SpringBootApplication: 标注这个类是一个springboot的应用，启动类下的所有资源被导入
+/**
+ * @author JBuck
+ * @SpringBootApplication: 标注这个类是一个springboot的应用，启动类下的所有资源被导入
+ */
 @SpringBootApplication
 public class ItsWebApplication {
     // 将springboot应用启动
